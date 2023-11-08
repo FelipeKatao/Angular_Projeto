26c97a403261c1f40414710717c8600754cd5c8f
FelipeKatao <cataofelipe@gmail.com>
2023-11-08 10:19:46 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,75 +1,83 @@
 
-var app = angular.module('blog',['ngRoute'])
+var app = angular.module('blog', ['ngRoute'])
 
 //Criação de suas controllers
-app.controller('home', function($scope) {
-    $scope.nome = "artigos"
+app.controller('home', function ($scope) {
+  $scope.nome = "artigos"
 });
 
 
 
-app.controller('artigo',function($scope,$http){
-  $scope.teste = function(Local){
-    alert(Local)
+app.controller('artigo', function ($scope, $http) {
+
+  //Funcao de clique
+  $scope.teste = function (TituloArtigo) {
+    alert(TituloArtigo)
   }
-  $scope.Lista_Add = ['ola']
-  var promise = $http.get("../data/artigos.json")
-  .then(function(response){
-  response.data.forEach(element => {
-    $scope.Lista_Add.push(element)
-  });
- })
+
+  // Lista para armazenar o Json
+  $scope.Lista_Add = []
+  // Ler o Json criado
+  var Lista_ler = $http.get("../data/artigos.json")
+    .then(function (response) {
+      response.data.forEach(element => {
+        //Adicionando os elementos do Json para a Lista
+        $scope.Lista_Add.push(element)
+      });
+
+    })
 
 })
 
 //Criação de suas controllers
-app.controller('artigos_base', function($scope) {
+app.controller('artigos_base', function ($scope) {
 
-    //Promisse para verificar os artigos inseridos na pagina
+  //Promisse para verificar os artigos inseridos na pagina
 
-    $scope.PromFunc = async function(){
-      const Res = await fetch('../data/artigos.json')
+  $scope.PromFunc = async function () {
+    const Res = await fetch('../data/artigos.json')
       .then(response => response.json())
       .then(json => {
         $scope.Artigos = json
-       for (let index = 0; index < $scope.Artigos.length; index++) {
-        document.getElementById("art1").innerHTML+=`
+        for (let index = 0; index < $scope.Artigos.length; index++) {
+          document.getElementById("art1").innerHTML += `
         <div class='Artigo_sq'>
-          <h1>`+$scope.Artigos[index]["Titulo"]+`</h1>
-          <h3> Autor: `+$scope.Artigos[index]["Autor"]+`</h3>
-          <div class ='premissa' >`+$scope.Artigos[index]["Premissa"]+`</div>
+          <h1>`+ $scope.Artigos[index]["Titulo"] + `</h1>
+          <h3> Autor: `+ $scope.Artigos[index]["Autor"] + `</h3>
+          <div class ='premissa' >`+ $scope.Artigos[index]["Premissa"] + `</div>
           <button>Ver artigo</button>
         </div> `
-       }})
-    }
+        }
+      })
+  }
 
-    //Remover o Anuncio inicial no Blog
-    $scope.CloseFunc = function(){
-     document.getElementsByClassName("mini_screen")[0].remove()
-     document.getElementsByClassName("Black_screen")[0].remove()
-    }
+  //Remover o Anuncio inicial no Blog
+  $scope.CloseFunc = function () {
+    document.getElementsByClassName("mini_screen")[0].remove()
+    document.getElementsByClassName("Black_screen")[0].remove()
+  }
 
-    //Chama a função da Promisse
-     $scope.PromFunc()
-     $scope.CloseFunc()
-     //window.location.href ="http://127.0.0.1:5500/index.html#!/artigos"
+  //Chama a função da Promisse
+  $scope.PromFunc()
+  $scope.CloseFunc()
+  //window.location.href ="http://127.0.0.1:5500/index.html#!/artigos"
 
 });
 
 //Configuração de criação de Rotas
-app.config(function($routeProvider) {
-    $routeProvider
+app.config(function ($routeProvider) {
+  $routeProvider
     .when("/", {
-        templateUrl : "./templates/main.html",
-        controller:"artigos_base"
-      })
+      templateUrl: "./templates/main.html",
+      controller: "artigos_base"
+    })
     .when("/artigos", {
-        templateUrl : "./templates/artigos.html",
-        controller:"artigo"
-      })
-    .when("/leitura/:art",{
-      templateUrl : "./templates/leitura.html",
-        controller:"artigo"
+      templateUrl: "./templates/artigos.html",
+      controller: "artigo"
+    })
+    .when("/leitura/:art", {
+      templateUrl: "./templates/leitura.html",
+      controller: "artigo"
     })
 
-  });
+});
