ba473ef86415e4039ba119a5cde8d45a5e76664b
FelipeKatao <cataofelipe@gmail.com>
2023-12-12 08:52:54 -0300

    update

---
+++ teste/app.py
@@ -0,0 +1,4 @@
+Var =str("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
+print(Var)
+print("##########'")
+print(Var.replace('k','m').replace('o','q').replace('e','g'))
